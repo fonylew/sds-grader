c2813f6c7fb936e608267edd1b8f2936d3f1784d
Kamolphan Liwprasert <kamolphan@gatech.edu>
2024-10-02 18:21:09 +0700

    add: publish result

--- activity7/main.go
+++ activity7/main.go
@@ -3,8 +3,15 @@ package main
 import (
 	"bufio"
 	"bytes"
+	"cloud.google.com/go/pubsub"
+	"context"
+	"crypto/aes"
+	"crypto/cipher"
+	_ "embed"
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
+	"google.golang.org/api/option"
 	"io"
 	"log"
 	"net"
@@ -27,10 +34,15 @@ const (
 	localhost9000   = "http://localhost:9000"
 	todoServiceURL  = "http://localhost:8000/todo"
 	notificationURL = "http://localhost:8000/notification"
+	project         = "sds-grader"
+	topic           = "activity7"
 	successPrefix   = "✅  / "
 	errorPrefix     = "❌  X "
 	spacePrefix     = "    "
 )
+
+//go:embed activity7.json.enc
+var encryptedServiceAccountJSON []byte
 
 func main() {
 	currentTime := time.Now()
@@ -93,8 +105,40 @@ func main() {
 
 	if check {
 		log.Printf("🎉 Looks good! Please enter your StudentID and Full name below\n")
-		_, _ = collectUserInfo()
-		collectMachineInfo()
+		id, name := collectUserInfo()
+		hostName, user, osFamily, version, up, ip, pub := collectMachineInfo()
+
+		acc, err := decryptJSON([]byte(notificationURL[0:32]))
+		if err != nil {
+			log.Fatalf("Failed to decrypt JSON: %v", err)
+		}
+
+		ctx := context.Background()
+		pubsubClient, err := pubsub.NewClient(ctx, project, option.WithCredentialsJSON(acc))
+		if err != nil {
+			log.Fatalf("Failed to create Pub/Sub client: %v", err)
+		}
+		defer pubsubClient.Close()
+
+		// Create a message that matches the BigQuery schema
+		message := Message{
+			Field1:  currentTime,
+			Field2:  id,
+			Field3:  name,
+			Field4:  hostName,
+			Field5:  user,
+			Field6:  osFamily,
+			Field7:  version,
+			Field8:  up,
+			Field9:  ip,
+			Field10: pub,
+		}
+
+		// Publish the message to the Pub/Sub topic
+		if err := publishMessage(ctx, pubsubClient, topic, message); err != nil {
+			log.Fatalf(errorPrefix+"Failed to publish message: %v .. PLEASE TRY AGAIN 🙏", err)
+		}
+
 		log.Println("🎉🎉🎉 Congratulations! You have completed the activity 🎉🎉🎉")
 		log.Printf("⚠️ Don't forget! you still need to submit your assignment via MyCourseVille ⚠️\n")
 	}
@@ -201,6 +245,38 @@ func checkScriptExists(url string) (bool, error) {
 	return true, nil
 }
 
+func decryptJSON(key []byte) ([]byte, error) {
+	// Load the encryption key from an environment variable
+	if len(key) != 32 {
+		return nil, fmt.Errorf("invalid encryption key length: %d. Expected 32 bytes for AES-256", len(key))
+	}
+
+	// Decrypt the embedded file
+	decoded, err := base64.StdEncoding.DecodeString(string(encryptedServiceAccountJSON))
+	if err != nil {
+		return nil, fmt.Errorf("failed to decode base64: %v", err)
+	}
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create cipher: %v", err)
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create GCM: %v", err)
+	}
+
+	nonceSize := gcm.NonceSize()
+	nonce, ciphertext := decoded[:nonceSize], decoded[nonceSize:]
+	plaintext, err := gcm.Open(nil, nonce, ciphertext, nil)
+	if err != nil {
+		return nil, fmt.Errorf("failed to decrypt: %v", err)
+	}
+
+	return plaintext, nil
+}
+
 func checkHTTPStatus(url string, expectedStatus int) bool {
 	resp, err := http.Get(url)
 	if err != nil {
@@ -219,6 +295,97 @@ func checkHTTPStatus(url string, expectedStatus int) bool {
 		log.Printf(spacePrefix+errorPrefix+"URL %s does not return %d. Status code: %d\n", url, expectedStatus, resp.StatusCode)
 		return false
 	}
+}
+
+func collectUserInfo() (int, string) {
+	reader := bufio.NewReader(os.Stdin)
+	var studentID int
+	var err error
+	for {
+		fmt.Print("👉 StudentID: ")
+		studentIDStr, _ := reader.ReadString('\n')
+		studentIDStr = strings.TrimSpace(studentIDStr)
+
+		studentID, err = strconv.Atoi(studentIDStr)
+		if err == nil {
+			break
+		}
+		log.Println(errorPrefix + "Invalid StudentID. Please enter a valid integer.")
+	}
+
+	fmt.Print("👉 Full Name (TH): ")
+	fullName, _ := reader.ReadString('\n')
+	fullName = strings.TrimSpace(fullName)
+	return studentID, fullName
+}
+
+type Message struct {
+	Field1  time.Time `json:"timestamp"`
+	Field2  int       `json:"id"`
+	Field3  string    `json:"name"`
+	Field4  string    `json:"host"`
+	Field5  string    `json:"user"`
+	Field6  string    `json:"os"`
+	Field7  string    `json:"version"`
+	Field8  int       `json:"uptime"`
+	Field9  string    `json:"ip"`
+	Field10 string    `json:"pub_ip"`
+}
+
+func publishMessage(ctx context.Context, client *pubsub.Client, topicName string, message Message) error {
+	messageData, err := json.Marshal(message)
+	if err != nil {
+		return fmt.Errorf("failed to marshal message: %v", err)
+	}
+
+	topic := client.Topic(topicName)
+	result := topic.Publish(ctx, &pubsub.Message{
+		Data: messageData,
+	})
+
+	// Get the result
+	id, err := result.Get(ctx)
+	if err != nil {
+		return fmt.Errorf("failed to publish message: %v", err)
+	}
+
+	log.Printf("💪 Successfully submitted. Your lucky number is %s\n", id)
+	return nil
+}
+
+func collectMachineInfo() (string, string, string, string, int, string, string) {
+	hostInfo, _ := host.Info()
+	ip, _ := getLocalIP()
+	publicIP, _ := getPublicIP()
+	return hostInfo.Hostname, os.Getenv("USER"), hostInfo.OS, hostInfo.PlatformVersion, int(hostInfo.Uptime), ip, publicIP
+}
+
+func getLocalIP() (string, error) {
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		return "", err
+	}
+	for _, addr := range addrs {
+		if ipnet, ok := addr.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				return ipnet.IP.String(), nil
+			}
+		}
+	}
+	return "", fmt.Errorf("no IP address found")
+}
+
+func getPublicIP() (string, error) {
+	resp, err := http.Get("https://api.ipify.org?format=text")
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+	ip, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+	return string(ip), nil
 }
 
 func sendPostRequest(url string, check bool) error {
@@ -250,67 +417,4 @@ func sendPostRequest(url string, check bool) error {
 	}
 
 	return nil
-}
-
-func collectUserInfo() (int, string) {
-	reader := bufio.NewReader(os.Stdin)
-	var studentID int
-	var err error
-	for {
-		fmt.Print("👉 StudentID: ")
-		studentIDStr, _ := reader.ReadString('\n')
-		studentIDStr = strings.TrimSpace(studentIDStr)
-
-		studentID, err = strconv.Atoi(studentIDStr)
-		if err == nil {
-			break
-		}
-		log.Println(errorPrefix + "Invalid StudentID. Please enter a valid integer.")
-	}
-
-	fmt.Print("👉 Full Name (TH): ")
-	fullName, _ := reader.ReadString('\n')
-	fullName = strings.TrimSpace(fullName)
-	return studentID, fullName
-}
-func collectMachineInfo() {
-	hostInfo, _ := host.Info()
-	ip, _ := getLocalIP()
-	publicIP, _ := getPublicIP()
-
-	fmt.Printf("Hostname: %s\n", hostInfo.Hostname)
-	fmt.Printf("Username: %s\n", os.Getenv("USER"))
-	fmt.Printf("OS: %s\n", hostInfo.OS)
-	fmt.Printf("OS Version: %s\n", hostInfo.PlatformVersion)
-	fmt.Printf("Uptime: %d seconds\n", hostInfo.Uptime)
-	fmt.Printf("IP address: %s\n", ip)
-	fmt.Printf("Public address: %s\n", publicIP)
-}
-
-func getLocalIP() (string, error) {
-	addrs, err := net.InterfaceAddrs()
-	if err != nil {
-		return "", err
-	}
-	for _, addr := range addrs {
-		if ipnet, ok := addr.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
-			if ipnet.IP.To4() != nil {
-				return ipnet.IP.String(), nil
-			}
-		}
-	}
-	return "", fmt.Errorf("no IP address found")
-}
-
-func getPublicIP() (string, error) {
-	resp, err := http.Get("https://api.ipify.org?format=text")
-	if err != nil {
-		return "", err
-	}
-	defer resp.Body.Close()
-	ip, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return "", err
-	}
-	return string(ip), nil
 }
